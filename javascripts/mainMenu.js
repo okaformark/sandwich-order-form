9fbd44548cffd0ba07b01e397ca8c6ea543000b7
Mark Okafor <okaformark@gmail.com>
2019-04-26 13:04:46 -0500

    styling WIP

--- javascripts/mainMenu.js
+++ javascripts/mainMenu.js
@@ -4,11 +4,11 @@ import util from './helpers/util.js';
 const makeSandwich = () => {
   let domString = '';
   buttonEvents.arrayMenu.forEach((menu) => {
-    domString += `<div class = " card-body row">`;
+    domString += `<div class = "row">`;
     domString += `<h1>${menu}</h1>`;
     domString += `</div>`;
   });
-  domString += `<h1>${buttonEvents.totalPriceGetter()}</h1>`;
+  domString += `<h1>Your Total is : $${buttonEvents.totalPriceGetter()}</h1>`;
   util.printToDom('cart', domString);
 }
 
